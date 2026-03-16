5727f6d14306966437689ce23708ef90e7b4e6db
pizzaispizza <99221666+pizzaispizza@users.noreply.github.com>
2026-03-16 23:51:15 +0800

    switch Appcues to NPM install

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+// start comment - installing Appcues via NPM
+import Appcues from "@appcues/web-sdk";
+// end comment
 import React from "react";
 import { BrowserRouter} from "react-router-dom";
 import { logEvents } from "./appcuesComponents/EventLog";
@@ -11,7 +14,10 @@ This is also where the Appcues.identify call is made, as well as any event liste
 like Appcues.on so that they will be called every time the page loads.
 */
 
-const App = () => {
+const App = () => { 
+  // npm install
+  await Appcues.setup('101304');
+  // end comment
   if (!window.localStorage.currentUser) { // persists the current user across page refresh
     window.localStorage.setItem("currentUser", "testUser"); // if no current user, set userID to "testUser"
   }
@@ -23,19 +29,19 @@ const App = () => {
   const userId = window.localStorage.currentUser;
   const currentTime = parseInt(window.localStorage.timestamp);
 
-  window.Appcues.identify(userId, { // feel free to add in any user properties here
+  Appcues.identify(userId, { // feel free to add in any user properties here
     role: "tester",
     signupDate: currentTime
   })
 
-  window.Appcues.on("all", function(name, payload) {
+  Appcues.on("all", function(name, payload) {
     // console.log(name);     // uncomment these two lines if you'd like to
     // console.log(payload);  // log all events into the dev tools console
 
     logEvents(name, payload); // adds events into the event log on the page
   })
 
-  window.Appcues.loadLaunchpad("#launchpad", { // loads Launchpad on this element
+  Appcues.loadLaunchpad("#launchpad", { // loads Launchpad on this element
     position: "right"
   })
   
